1e4de66e1295ccc45b955bcfcd0a7b4f40c5a405
LIM-SUVIN <pyotnqls25@naver.com>
2025-02-04 18:06:46 +0900

    feat: handlers 수정

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -12,24 +12,33 @@ export const handlers: HttpHandler[] = [
   }),
 
   http.post('/api/events', async ({ request }) => {
-    const { event } = (await request.json()) as { event: Event };
-    const newEvents = { ...event, id: String(events.length + 1) };
-    events.push(newEvents);
-    return HttpResponse.json(newEvents, { status: 201 });
+    let newEvent = (await request.json()) as Event;
+    newEvent = { ...newEvent, id: String(events.length + 1) };
+    return HttpResponse.json(newEvent, { status: 201 });
   }),
 
   http.put('/api/events/:id', async ({ params, request }) => {
     const { id } = params;
-    const updates = (await request.json()) as Record<string, unknown>;
-    const updateEvents = events.map((event) =>
-      event.id === id ? { ...event, ...updates } : event
-    );
-    return HttpResponse.json(updateEvents.find((event) => event.id === id));
+    let updateEvent = (await request.json()) as Event;
+    const targetEvent = events.findIndex((event) => event.id === id);
+
+    if (targetEvent !== -1) {
+      events[targetEvent] = { ...events[targetEvent], ...updateEvent };
+      return HttpResponse.json(events[targetEvent]);
+    }
+
+    return new HttpResponse(null, { status: 404 });
   }),
 
   http.delete('/api/events/:id', ({ params }) => {
     const { id } = params;
-    events.filter((event) => event.id !== id);
-    return new HttpResponse(null, { status: 204 });
+    const targetEvent = events.findIndex((event) => event.id === id);
+
+    if (targetEvent !== -1) {
+      events.splice(targetEvent, 1);
+      return new HttpResponse(null, { status: 204 });
+    }
+
+    return new HttpResponse(null, { status: 404 });
   }),
 ];
