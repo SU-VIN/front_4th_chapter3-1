1153c17a31bdd92492de5d09a770075f96f9a375
LIM-SUVIN <pyotnqls25@naver.com>
2025-02-05 18:24:17 +0900

    feat: medium hook  test 작성

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -13,6 +13,9 @@ import { Event, EventForm } from '../../types.ts';
 
 const initialEvents = events as Event[];
 
+const toast = vi.fn();
+vi.mock('@chakra-ui/react', () => ({ useToast: () => toast }));
+
 it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
   const { result } = renderHook(() => useEventOperations(false));
 
@@ -155,8 +158,104 @@ it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', 
   });
 });
 
-it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
+it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
+  server.use(
+    http.get('/api/events', () => {
+      return HttpResponse.json({ events }, { status: 500 });
+    })
+  );
 
-it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {});
+  renderHook(() => useEventOperations(false));
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+  await waitFor(() => {
+    expect(toast).toBeCalledWith({
+      title: '이벤트 로딩 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+});
+
+it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
+  const initEvents: Event[] = [
+    {
+      id: '1',
+      title: '기존 회의1',
+      date: '2024-10-15',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+    {
+      id: '2',
+      title: '기존 회의2',
+      date: '2024-10-16',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ];
+  setupMockHandlerUpdating(initEvents);
+
+  const updatedEvent: Event = {
+    id: '3',
+    title: '없는 이벤트 수정 테스트~',
+    date: '2024-10-15',
+    startTime: '09:00',
+    endTime: '11:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  };
+
+  const { result } = renderHook(() => useEventOperations(true));
+
+  await waitFor(() => {
+    expect(result.current.events).toEqual(initEvents);
+  });
+
+  act(() => {
+    result.current.saveEvent(updatedEvent);
+  });
+
+  await waitFor(() => {
+    expect(toast).toBeCalledWith({
+      title: '일정 저장 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+});
+
+it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+  server.use(
+    http.delete('/api/events/:id', () => {
+      return HttpResponse.error();
+    })
+  );
+
+  const { result } = renderHook(() => useEventOperations(false));
+
+  await result.current.deleteEvent('1');
+
+  await waitFor(() => {
+    expect(toast).toBeCalledWith({
+      title: '일정 삭제 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+});
