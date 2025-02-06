07a1340a8cedd4a15ad7f23df7dea2e31682b4e4
LIM-SUVIN <pyotnqls25@naver.com>
2025-02-06 11:38:04 +0900

    fix: 린트 에러 수정

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { render, screen, within } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
+
 import {
   setupMockHandlerCreation,
   setupMockHandlerDeletion,
